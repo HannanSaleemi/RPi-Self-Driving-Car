eaaea9769c5e3139792a418503f9c2819fbc95f6
Hannan Saleemi <hannansaleemi1@gmail.com>
2017-10-21 10:03:24 +0100

    Picamera code added for capture

--- Client-Server Test/Test 2/client.py
+++ Client-Server Test/Test 2/client.py
@@ -1,17 +1,22 @@
-import socket              
+import socket
+import picamera
+
+camera = picamera.PiCamera()
+
+camera.capture('tosend.png')
 
 s = socket.socket()         
                
-s.connect(('localhost', 12347))
+s.connect(('192.168.60', 12347))
 f = open('tosend.png','rb')
 print('Sending...')
-l = f.read(150000)
+l = f.read(200000)
 while (l):
     print('Sending...')
     s.send(l)
-    l = f.read(150000)
+    l = f.read(200000)
 f.close()
 print("Done Sending")
 s.shutdown(socket.SHUT_WR)
-print(s.recv(150000))
+print(s.recv(200000))
 s.close()
