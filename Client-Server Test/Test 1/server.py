c2e3afabfeba6e77cd7ca7c1258f7b58c5784f6a
Hannan Saleemi <hannansaleemi@Hannans-MacBook-Pro.local>
2017-10-21 09:50:18 +0100

    Server Changes

--- Client-Server Test/Test 1/server.py
+++ Client-Server Test/Test 1/server.py
@@ -1,9 +1,11 @@
 import socket               
+import cv
+import numpy as np
 
 s = socket.socket()         
 host = 'localhost' 
 port = 12347                 
-s.bind(('localhost', port))        
+s.bind(('192.168.0.60', port))        
 f = open('torecv.png','wb')
 s.listen(1)                 
 try:
@@ -23,22 +25,10 @@ try:
         break
 
     #Neural Network Predition START
-    from keras.models import load_model
-    classifier = load_model("/Users/intern.mac/Desktop/RPi-Self-Driving-Car/Trained Models/28-july-car_cnn.h5")
-
-    import numpy as np
-    from keras.preprocessing import image
-
-    test_image = image.load_img('torecv.png',  target_size=(64, 64))
-
-    test_image = image.img_to_array(test_image)
-
-    test_image = np.expand_dims(test_image, axis=0)
-
-    result = classifier.predict(test_image)
-
-    print result
-    #Neural Network Prediction END
+    
+    #Stop Sign
+    
+    #Traffic Light Threaded
     
 except KeyboardInterrupt:
     c.close()
