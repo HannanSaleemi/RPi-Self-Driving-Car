6c27abed43508b130637070736fa7f76b73d6be0
Hannan Saleemi <hannansaleemi1@gmail.com>
2017-10-21 11:10:53 +0100

    Server - moved all stop and traffic into seperate functions ready for threading

--- Client-Server Test/Test 2/server.py
+++ Client-Server Test/Test 2/server.py
@@ -20,6 +20,31 @@ def stopDetection():
         roi_gray = gray[y:y+h, x:x+w]
         stopPresent = True
 
+def trafficLightDetection():
+    trafficCascade = cv2.CascadeClassifier('traffic.xml')
+    img = cv2.imread('torecv.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    traffic = trafficCascade.detectMultiScale(gray, 1.1, 2)
+
+    for (x,y,w,h) in traffic:
+        print("Detected Traffic")
+        #cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+
+        newgray = cv2.GaussianBlur(roi_gray, (41, 41), 0)
+        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(newgray)
+
+        print(maxLoc)
+        print(maxLoc[0])
+        print(maxLoc[1])
+
+        if maxLoc[1] >= 230:
+            print("Green Detected")
+            greenPresent = True
+        elif maxLoc[1] >= 100:
+            print("Red Detected")
+            redPresent = True
+
 
 
 s = socket.socket()         
@@ -45,31 +70,6 @@ try:
         break
 
     #Neural Network Predition START
-    
-    #Traffic Light Threaded
-    trafficCascade = cv2.CascadeClassifier('traffic.xml')
-    img = cv2.imread('torecv.png')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    traffic = trafficCascade.detectMultiScale(gray, 1.1, 2)
-
-    for (x,y,w,h) in traffic:
-        print("Detected Traffic")
-        #cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-
-        newgray = cv2.GaussianBlur(roi_gray, (41, 41), 0)
-        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(newgray)
-
-        print(maxLoc)
-        print(maxLoc[0])
-        print(maxLoc[1])
-
-        if maxLoc[1] >= 230:
-            print("Green Detected")
-            greenPresent = True
-        elif maxLoc[1] >= 100:
-            print("Red Detected")
-            redPresent = True
             
     
 except KeyboardInterrupt:
